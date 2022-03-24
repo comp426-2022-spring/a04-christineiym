b79f9a90c1de119cd8868b13b91363050a025d1a
Christine Mendoza <christine.mendoza@unc.edu>
2022-03-24 08:30:37 -0400

    make type module and add help

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //// Creating the server ////
 // Define named constants
 const START_ARG_NUM = 2
+const EXIT_SUCCESS = 0
 const DEFAULT_PORT = 3000
 const HTTP_STATUS_OK = 200
 const HTTP_STATUS_NOT_FOUND = 404
@@ -12,30 +13,45 @@ const TAILS = 'tails'
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url)
 
-// Require Express.js
-const express = require('express')
-const app = express()
-
-// Require minimist module to process one argument `--port=` on the command line after `node server.js`.
+// Require minimist module to process arguments.
 const minimist = require('minimist')
 const { exit } = require('process')
-
-// Define allowed argument name 'port'.
 const allArguments = minimist(process.argv.slice(START_ARG_NUM))
-const argPort = allArguments['port']
+
+// Print help message if asked for.
+if (allArguments['help']) {
+    console.log(`server.js [options]
+
+    --port	Set the port number for the server to listen on. Must be an integer
+                between 1 and 65535.
+  
+    --debug	If set to \`true\`, creates endlpoints /app/log/access/ which returns
+                a JSON access log from the database and /app/error which throws 
+                an error with the message "Error test successful." Defaults to 
+                \`false\`.
+  
+    --log		If set to false, no log files are written. Defaults to true.
+                Logs are always written to database.
+  
+    --help	Return this message and exit.`)
+    exit(EXIT_SUCCESS)
+}
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = argPort || process.env.PORT || DEFAULT_PORT
+const port = allArguments['port'] || process.env.PORT || DEFAULT_PORT
 
 // Import the coinFlips and countFlips functions from your coin.mjs file
 import { coinFlip, coinFlips, countFlips, flipACoin } from './coin.mjs'
+
+// Require Express.js
+const express = require('express')
+const app = express()
 
 // Start an app server
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
 })
-
 
 //// API endpoints ////
 // Check endpoint
